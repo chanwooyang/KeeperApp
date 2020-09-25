475075b23d223e30e81d2ee5c3b079484d65e574
chanwooyang <chanwoo.m.yang@gmail.com>
2020-09-25 14:28:36 -0700

    material-ui is added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = React.useState(false);
+
   const [newNote, setNewNote] = React.useState({
     title: "",
     content: "",
@@ -10,18 +15,6 @@ function CreateArea(props) {
     const { name, value } = event.target;
 
     setNewNote((prevValue) => {
-      //   if (name === "title") {
-      //     return {
-      //       title: value,
-      //       content: prevValue.content,
-      //     };
-      //   } else if (name === "content") {
-      //     return {
-      //       title: prevValue.title,
-      //       content: value,
-      //     };
-      //   }
-
       return { ...prevValue, [name]: value };
     });
   }
@@ -35,25 +28,34 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input
-          onChange={handleChange}
-          name="title"
-          placeholder="Title"
-          value={newNote.title}
-        />
+      <form className="create-note">
+        {isExpanded ? (
+          <input
+            onChange={handleChange}
+            name="title"
+            placeholder="Title"
+            value={newNote.title}
+          />
+        ) : null}
         <textarea
+          onClick={expand}
           onChange={handleChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
           value={newNote.content}
         />
-        <button type="submit" onClick={handleClick}>
-          Add
-        </button>
+        <Zoom in={isExpanded}>
+          <Fab type="submit" onClick={handleClick}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
